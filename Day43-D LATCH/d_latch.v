6001236be1e2291b1b52d3efcdf4ce296d0216ba
Nikita Dhuppe <110588643+niku41@users.noreply.github.com>
2024-09-15 18:30:22 +0530

    Add files via upload

---
+++ Day43-D LATCH/d_latch.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 16:18:22
+// Design Name: 
+// Module Name: d_latch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_latch(
+
+    );
+endmodule
