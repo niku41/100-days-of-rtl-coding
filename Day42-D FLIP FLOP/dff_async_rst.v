827e645f78252500cbd951a24735835d1bfad3d2
Nikita Dhuppe <110588643+niku41@users.noreply.github.com>
2024-09-15 18:30:11 +0530

    Add files via upload

---
+++ Day42-D FLIP FLOP/dff_async_rst.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 16:19:48
+// Design Name: 
+// Module Name: dff_async_rst
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff_async_rst(
+
+    );
+endmodule
---
+++ Day42-D FLIP FLOP/dff_sync_rst.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 16:20:05
+// Design Name: 
+// Module Name: dff_sync_rst
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff_sync_rst(
+
+    );
+endmodule
